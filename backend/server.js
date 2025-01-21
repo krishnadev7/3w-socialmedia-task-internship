9bf4c15a4eea726097ad0f051cc1148235b0c978
krishnadev7 <krishnadevv12@gmail.com>
2025-01-21 20:59:03 +0530

    added cors policy in backend

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
-import bodyParser from 'body-parser'
+import bodyParser from "body-parser";
 import { User } from "./models/User.js";
 
 const app = express();
@@ -11,10 +11,17 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 dotenv.config();
-app.use(cors());
 const port = 8000 || process.env.PORT;
-app.use('/uploads', express.static('uploads'));
 
+const corsOptions = {
+  origin: "https://radiant-dodol-0888f0.netlify.app",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+app.use("/uploads", express.static("uploads"));
 
 //Mongodb Connection Setup
 mongoose
@@ -22,35 +29,35 @@ mongoose
   .then(() => console.log("Mongodb Connected successfully"))
   .catch((error) => console.error(error));
 
-app.get('/', (req,res) => {
-    res.send("hello world");
-})
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
-app.post('/submit', upload.array('images', 5), async (req, res) => {
-    const { name, socialHandle } = req.body;
-    
-    const imagePaths = req.files.map(file => `/uploads/${file.filename}`);
-    try {
-      const newUser = new User({ name, socialHandle,images: imagePaths});
-     const saveduser =  await newUser.save();
-      res.status(201).json({ message: 'User data saved successfully',saveduser });
-    } catch (error) {
-      res.status(500).json({ error: 'Failed to save user data' });
-    }
-  });
-  
-  // Route to fetch all submissions
-  app.get('/submissions', async (req, res) => {
-    try {
-      const users = await User.find();
-      res.json(users);
-    } catch (error) {
-      res.status(500).json({ error: 'Failed to fetch submissions' });
-    }
-  });
+app.post("/submit", upload.array("images", 5), async (req, res) => {
+  const { name, socialHandle } = req.body;
 
+  const imagePaths = req.files.map((file) => `/uploads/${file.filename}`);
+  try {
+    const newUser = new User({ name, socialHandle, images: imagePaths });
+    const saveduser = await newUser.save();
+    res
+      .status(201)
+      .json({ message: "User data saved successfully", saveduser });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to save user data" });
+  }
+});
 
+// Route to fetch all submissions
+app.get("/submissions", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch submissions" });
+  }
+});
 
-  app.listen(port, () =>
-    console.log(`server running on http://localhost:${port}`)
-  );
+app.listen(port, () =>
+  console.log(`server running on http://localhost:${port}`)
+);
